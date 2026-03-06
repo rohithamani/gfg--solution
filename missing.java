52cd708e8132c072e11da0c365d8947c4481aa7a
rohithamani <rohithamani22@gmail.com>
2026-03-06 23:32:37 +0530

    Add files via upload

---
+++ missing.java
@@ -0,0 +1,12 @@
+class Solution {
+    int missingNum(int arr[]) {
+        // code here
+        Arrays.sort(arr);
+        for(int i = 0;i< arr.length;i++){
+            if(arr[i]!=i+1){
+                return i+1;
+            }
+        }
+    return arr.length+1;
+}
+}
